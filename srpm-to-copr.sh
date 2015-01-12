f5598e9d2aa4d0907596a32cab773799d522f370
Miguel Angel Ajo <miguelangel@ajo.es>
2015-01-12 18:17:23 +0100

    updated srpm-to-copr to handle ci.kicad-pcb.org

--- srpm-to-copr.sh
+++ srpm-to-copr.sh
@@ -2,8 +2,11 @@
 set -x
 set -e
 
-SRPM_FILE=$(fedpkg srpm | grep Wrote | cut -d\  -f 2)
+SRPM_FILE=$(fedpkg --dist master srpm | grep Wrote | cut -d\  -f 2)
 scp $SRPM_FILE mangelajo@fedorapeople.org:~/public_html/copr/
 SRPM_FILENAME=$(basename $SRPM_FILE)
-copr-cli build kicad https://mangelajo.fedorapeople.org/copr/$SRPM_FILENAME
-
+if [ "$( whoami )"=="jenkins" ]; then
+        copr-cli build kicad http://ci.kicad-pcb.org/job/fedora-nightlies/ws/$SRPM_FILENAME
+else
+        copr-cli build kicad https://mangelajo.fedorapeople.org/copr/$SRPM_FILENAME
+fi
