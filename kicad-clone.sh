26abccf4c607b59ade87205210b362ee6ed399fc
Steven A. Falco <stevenfalco@gmail.com>
2018-02-26 21:29:25 -0500

    Revise libraries for kicad 5

--- kicad-clone.sh
+++ kicad-clone.sh
@@ -10,31 +10,49 @@ else
 	git clone https://git.launchpad.net/kicad kicad.bzr
 fi
 
-if [ -d kicad-library.bzr ]; then
-	cd kicad-library.bzr
+if [ -d kicad-symbols.bzr ]; then
+	cd kicad-symbols.bzr
 	git fetch origin
 	git reset --hard origin/master
 	cd ..
 else
-	git clone https://github.com/KiCad/kicad-library.git kicad-library.bzr
+	git clone https://github.com/KiCad/kicad-symbols.git kicad-symbols.bzr
+fi
+
+if [ -d kicad-footprints.bzr ]; then
+	cd kicad-footprints.bzr
+	git fetch origin
+	git reset --hard origin/master
+	cd ..
+else
+	git clone https://github.com/KiCad/kicad-footprints.git kicad-footprints.bzr
+fi
+
+if [ -d kicad-packages3D.bzr ]; then
+	cd kicad-packages3D.bzr
+	git fetch origin
+	git reset --hard origin/master
+	cd ..
+else
+	git clone https://github.com/KiCad/kicad-packages3D.git kicad-packages3D.bzr
+fi
+
+if [ -d kicad-templates.bzr ]; then
+	cd kicad-templates.bzr
+	git fetch origin
+	git reset --hard origin/master
+	cd ..
+else
+	git clone https://github.com/KiCad/kicad-templates.git kicad-templates.bzr
 fi
 
 if [ -d kicad-i18n ]; then
-    cd kicad-i18n
-    git fetch origin
-    git reset --hard origin/master
-    cd ..
+	cd kicad-i18n
+	git fetch origin
+	git reset --hard origin/master
+	cd ..
 else
-    git clone https://github.com/KiCad/kicad-i18n.git
-fi
-
-if [ -d kicad-footprints ]; then
-    cd kicad-footprints
-    git fetch origin
-    git reset --hard origin/master
-    cd ..
-else
-    git clone https://github.com/KiCad/kicad-footprints.git
+	git clone https://github.com/KiCad/kicad-i18n.git
 fi
 
 #TODO(mangelajo): pull the new doc builds
