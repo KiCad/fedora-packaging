d83501653496c48a2b1a8881e6715aecb268170d
Steven A. Falco <stevenfalco@gmail.com>
2018-02-26 21:29:25 -0500

    Revise libraries for kicad 5

--- kicad-export.sh
+++ kicad-export.sh
@@ -9,7 +9,7 @@ get_last_rev()
 }
 
 MAIN_REV=$(get_last_rev kicad.bzr)
-LIB_REV=$(get_last_rev kicad-library.bzr)
+#LIB_REV=$(get_last_rev kicad-library.bzr)
 #DOC_REV=$(get_last_rev kicad-doc.bzr)
 TIMESTAMP="$MAIN_REV"
 
@@ -21,18 +21,25 @@ git archive --format=tar.gz --prefix=kicad-$TIMESTAMP/ HEAD > ../kicad-$TIMESTAM
 #tar cJf ../kicad-$TIMESTAMP.tar.xz kicad-$TIMESTAMP
 #rm -rf kicad-$TIMESTAMP
 
-cd ../kicad-library.bzr
-echo "Creating kicad-libraries-$TIMESTAMP.tar.gz ..."
-git archive --format=tar.gz --prefix=kicad-libraries-$TIMESTAMP/ HEAD > ../kicad-libraries-$TIMESTAMP.tar.gz
+cd ../kicad-symbols.bzr
+echo "Creating kicad-symbols-$TIMESTAMP.tar.gz ..."
+git archive --format=tar.gz --prefix=kicad-symbols-$TIMESTAMP/ HEAD > ../kicad-symbols-$TIMESTAMP.tar.gz
 
+cd ../kicad-footprints.bzr
+echo "Creating kicad-footprints-$TIMESTAMP.tar.gz ..."
+git archive --format=tar.gz --prefix=kicad-footprints-$TIMESTAMP/ HEAD > ../kicad-footprints-$TIMESTAMP.tar.gz
+
+cd ../kicad-packages3D.bzr
+echo "Creating kicad-packages3D-$TIMESTAMP.tar.gz ..."
+git archive --format=tar.gz --prefix=kicad-packages3D-$TIMESTAMP/ HEAD > ../kicad-packages3D-$TIMESTAMP.tar.gz
+
+cd ../kicad-templates.bzr
+echo "Creating kicad-templates-$TIMESTAMP.tar.gz ..."
+git archive --format=tar.gz --prefix=kicad-templates-$TIMESTAMP/ HEAD > ../kicad-templates-$TIMESTAMP.tar.gz
 
 cd ../kicad-i18n
 echo "Creating kicad-i18n-$TIMESTAMP.tar.gz ..."
 git archive --format=tar.gz --prefix=kicad-i18n-$TIMESTAMP/ HEAD > ../kicad-i18n-$TIMESTAMP.tar.gz
-
-cd ../kicad-footprints
-echo "Creating kicad-footprints-$TIMESTAMP.tar.gz ..."
-git archive --format=tar.gz --prefix=kicad-footprints-$TIMESTAMP/ HEAD > ../kicad-footprints-$TIMESTAMP.tar.gz
 
 #TODO(mangelajo): new docs?
 #cd ../kicad-doc.bzr
